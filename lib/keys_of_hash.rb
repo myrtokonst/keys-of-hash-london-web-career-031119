558e265e4415166a6f4d518ebf198423236387fe
Myrto Konstantinidi <myrtokonst@gmail.com>
2019-03-10 10:28:47 +0000

    Automatically backed up by Learn

--- lib/keys_of_hash.rb
+++ lib/keys_of_hash.rb
@@ -1,11 +1,9 @@
 class Hash
   def keys_of(*arguments)
     common_keys = []
-    arguments.each do |argument|
       arguments.each do |key, value|
         if argument == value
           common_keys.push(value)
-        end 
       end 
     end
     return common_keys
